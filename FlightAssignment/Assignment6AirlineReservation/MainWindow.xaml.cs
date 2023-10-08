15f4d3f45085417d5584a13f9afb9be5130c9a76
ChrisYoungberg <70718100+ChrisYoungberg@users.noreply.github.com>
2023-10-07 20:19:35 -0600

    Add files via upload

---
+++ FlightAssignment/Assignment6AirlineReservation/MainWindow.xaml.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assignment6AirlineReservation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        clsMainLogic clsLogic;
+        wndAddPassenger wndAddPass;
+        ObservableCollection<clsPassenger> passengers;
+        bool bAddingPass;
+        bool bChangeSeat;
+
+        public MainWindow()
+        {
+            try
+            {
+                InitializeComponent();
+                Application.Current.ShutdownMode = ShutdownMode.OnMainWindowClose;
+                clsLogic = new clsMainLogic();
+                bAddingPass = false;
+
+                cbChooseFlight.ItemsSource = clsLogic.GetFlights();
+            }
+            catch (Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                    MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+        }
+
+        private void cbChooseFlight_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                string selection = cbChooseFlight.SelectedItem.ToString();
+                cbChoosePassenger.IsEnabled = true;
+                gPassengerCommands.IsEnabled = true;
+
+                passengers = clsLogic.GetPassengers(selection);
+                clsLogic.setFlight(selection);
+                cbChoosePassenger.ItemsSource = passengers;
+
+                if (selection == "1")
+                {
+                    CanvasA380.Visibility = Visibility.Hidden;
+                    Canvas767.Visibility = Visibility.Visible;
+
+                    for (int i = 0; i < passengers.Count; i++)
+                    {
+                        string sSeat = passengers[i].Seat;
+
+                        foreach (Label seat in c767_Seats.Children)
+                        {
+                            if (seat.Content.Equals(sSeat))
+                            {
+                                seat.Background = Brushes.Red;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Canvas767.Visibility = Visibility.Hidden;
+                    CanvasA380.Visibility = Visibility.Visible;
+
+                    for (int i = 0; i < passengers.Count; i++)
+                    {
+                        string sSeat = passengers[i].Seat;
+
+                        foreach (Label seat in cA380_Seats.Children)
+                        {
+                            if (seat.Content.Equals(sSeat))
+                            {
+                                seat.Background = Brushes.Red;
+                            }
+                        }
+                    }
+                }
+
+                
+
+            }
+            catch (Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                    MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+        }
+
+        private void cmdAddPassenger_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                wndAddPass = new wndAddPassenger();
+                wndAddPass.ShowDialog();
+                SelSeatForAdd();
+            }
+            catch (Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                    MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+        }
+
+        private void HandleError(string sClass, string sMethod, string sMessage)
+        {
+            try
+            {
+                MessageBox.Show(sClass + "." + sMethod + " -> " + sMessage);
+            }
+            catch (System.Exception ex)
+            {
+                System.IO.File.AppendAllText(@"C:\Error.txt", Environment.NewLine + "HandleError Exception: " + ex.Message);
+            }
+        }
+
+        private void cbChoosePassenger_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                string sSel = cbChoosePassenger.SelectedItem.ToString();
+                string sID = sSel.Substring(0, 1);
+                string sSeat = clsLogic.selectPass(cbChooseFlight.SelectedItem.ToString(), sID);
+                setSelectedBox(sSeat);
+            }
+            catch (Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                    MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+        }
+
+        private void SeatClicked(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                Label lbl = (Label)sender;
+
+                if (lbl.Background.Equals(Brushes.Green))
+                {
+
+                    string sID;
+                    sID = clsLogic.SeatSelected(cbChooseFlight.SelectedItem.ToString(), lbl.Content.ToString());
+
+                    foreach (ComboBoxItem pas in cbChoosePassenger.Items)
+                    {
+                        string sPasID = pas.Content.ToString().Substring(0, 1);
+
+                        if (sPasID.Equals(sID))
+                        {
+                            pas.IsSelected = true;
+                        }
+                    }
+
+                    setSelectedBox(lbl.Content.ToString());
+                }
+
+                if (lbl.Background.Equals(Brushes.Blue) && bAddingPass)
+                {
+                    clsLogic.AddLink(lbl.Content.ToString());
+                    passengers = clsLogic.GetPassengers();
+                    cbChoosePassenger.ItemsSource = passengers;
+                    PassAndLinkAdded();
+                    bAddingPass = false;
+                }
+
+                if (lbl.Background.Equals(Brushes.Blue) && bChangeSeat)
+                {
+                    clsLogic.ChangeSeat(lbl.Content.ToString());
+                    bChangeSeat = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                    MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+        }
+
+        private void setSelectedBox(string sel)
+        {
+            try
+            {
+                lblPassengersSeatNumber.Content = sel;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+
+        private void SelSeatForAdd()
+        {
+            try
+            {
+                cmdChangeSeat.IsEnabled = false;
+                cmdAddPassenger.IsEnabled = false;
+                cmdDeletePassenger.IsEnabled = false;
+                bAddingPass = true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+
+        private void PassAndLinkAdded()
+        {
+            try
+            {
+                cmdChangeSeat.IsEnabled = true;
+                cmdAddPassenger.IsEnabled = true;
+                cmdDeletePassenger.IsEnabled = true;
+                gbPassengerInformation.IsEnabled = true;
+                gPassengerCommands.IsEnabled = true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+
+        private void cmdDeletePassenger_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                clsLogic.DeletePassenger();
+                clsLogic.GetPassengers();
+            }
+            catch (Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                    MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+        }
+
+        private void cmdChangeSeat_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                gbPassengerInformation.IsEnabled = false;
+                gPassengerCommands.IsEnabled = false;
+                bChangeSeat = true;
+            }
+            catch (Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                    MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+        }
+    }
+}
---
+++ FlightAssignment/Assignment6AirlineReservation/clsMainLogic.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6AirlineReservation
+{ 
+    public class clsMainLogic
+    {
+        private clsDataAccess clsData;
+        private clsMainSQL clsSQL;
+        private clsFlight Flight;
+        public ObservableCollection<clsFlight> flights;
+        public ObservableCollection<clsPassenger> passengers;
+        private string sSelPassID;
+        private string sSelFlightID;
+        private string sFNameTemp;
+        private string sLNameTemp;
+
+        public clsMainLogic()
+        {
+            try
+            {
+                clsData = new clsDataAccess();
+                clsSQL = new clsMainSQL();
+                flights = new ObservableCollection<clsFlight>();
+                passengers = new ObservableCollection<clsPassenger>();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+
+        public ObservableCollection<clsFlight> GetFlights()
+        {
+            try
+            {
+                DataSet ds = new DataSet();
+                int iRet = 0;
+                clsData = new clsDataAccess();
+
+                try
+                {
+                    ds = clsData.ExecuteSQLStatement(clsSQL.GetFlights(), ref iRet);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+                }
+
+                for (int i = 0; i < iRet; i++)
+                {
+                    Flight = new clsFlight();
+                    Flight.ID = ds.Tables[0].Rows[i][0].ToString();
+                    Flight.Number = ds.Tables[0].Rows[i][1].ToString();
+                    Flight.Type = ds.Tables[0].Rows[i][2].ToString();
+
+                    flights.Add(Flight);
+                }
+
+                return flights;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+
+        public ObservableCollection<clsPassenger> GetPassengers(string sel)
+        {
+            try
+            {
+                int iRet = 0;
+                DataSet ds = new DataSet();
+                clsPassenger Passenger;
+                passengers.Clear();
+
+                try
+                {
+                    ds = clsData.ExecuteSQLStatement(clsSQL.GetPassengers(sel), ref iRet);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+                }
+
+                for (int i = 0; i < iRet; i++)
+                {
+                    Passenger = new clsPassenger();
+                    Passenger.ID = ds.Tables[0].Rows[i][0].ToString();
+                    Passenger.FirstName = ds.Tables[0].Rows[i]["First_Name"].ToString();
+                    Passenger.LastName = ds.Tables[0].Rows[i]["Last_Name"].ToString();
+                    Passenger.Flight = ds.Tables[0].Rows[i][3].ToString();
+                    Passenger.Seat = ds.Tables[0].Rows[i][4].ToString();
+
+                    passengers.Add(Passenger);
+                }
+
+                return passengers;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+
+        public ObservableCollection<clsPassenger> GetPassengers()
+        {
+            try
+            {
+                int iRet = 0;
+                DataSet ds = new DataSet();
+                clsPassenger Passenger;
+                passengers.Clear();
+
+                try
+                {
+                    ds = clsData.ExecuteSQLStatement(clsSQL.GetPassengers(sSelFlightID), ref iRet);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+                }
+
+                for (int i = 0; i < iRet; i++)
+                {
+                    Passenger = new clsPassenger();
+                    Passenger.ID = ds.Tables[0].Rows[i][0].ToString();
+                    Passenger.FirstName = ds.Tables[0].Rows[i]["First_Name"].ToString();
+                    Passenger.LastName = ds.Tables[0].Rows[i]["Last_Name"].ToString();
+                    Passenger.Flight = ds.Tables[0].Rows[i][3].ToString();
+                    Passenger.Seat = ds.Tables[0].Rows[i][4].ToString();
+
+                    passengers.Add(Passenger);
+                }
+
+                return passengers;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+
+        public string selectPass(string flight, string ID)
+        {
+            try
+            {
+                sSelPassID = ID;
+
+                string sRet;
+                sRet = clsData.ExecuteScalarSQL(clsSQL.GetPassSeat(flight, ID));
+
+                return sRet;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+
+        public string SeatSelected(string Flight, string seat)
+        {
+            try
+            {
+                string sRet;
+                sRet = clsData.ExecuteScalarSQL(clsSQL.GetPassID(Flight, seat));
+
+                sSelPassID = sRet;
+
+                return sSelPassID;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+
+        public void setFlight(string ID)
+        {
+            try
+            {
+                sSelFlightID = ID;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+
+        public void AddPass(string fname, string lname)
+        {
+            try
+            {
+                int iRet;
+                iRet = clsData.ExecuteNonQuery(clsSQL.AddPass(fname, lname));
+                sFNameTemp = fname;
+                sLNameTemp = lname;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+
+        public void AddLink(string seat)
+        {
+            try
+            {
+                string sID = clsData.ExecuteScalarSQL(clsSQL.GetPassIDPass(sFNameTemp, sLNameTemp));
+                int iRet;
+                iRet = clsData.ExecuteNonQuery(clsSQL.AddLink(sSelFlightID, sID, seat));
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+
+        public void DeletePassenger()
+        {
+            int iRet;
+            try
+            {
+                iRet = clsData.ExecuteNonQuery(clsSQL.DeleteLink(sSelFlightID, sSelPassID));
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+            try
+            {
+                iRet = clsData.ExecuteNonQuery(clsSQL.DeletePassenger(sSelPassID));
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+
+        public void ChangeSeat(string newseat)
+        {
+            try
+            {
+                int iRet;
+                iRet = clsData.ExecuteNonQuery(clsSQL.ChangeSeat(newseat, sSelFlightID, sSelPassID));
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+    }
+}
